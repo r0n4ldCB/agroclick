130bca35a4b4ca5d7a877ea671aaa093a3ea9f54
r0n4ldCB <35352167+r0n4ldCB@users.noreply.github.com>
2024-01-15 21:16:10 -0600

    otro fix a github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://r0n4ldcb.github.io/agroclick/',
+  site: 'https://r0n4ldcb.github.io',
   base: '/agroclick',
 });
